3dc1a541c777dcd6731dde3813a2a16a7a8ca1ad
Sam <142849975+07038@users.noreply.github.com>
2023-10-07 18:27:12 +0100

    Add files via upload

---
+++ JAVASCRIPT/index.js
@@ -0,0 +1,139 @@
+// Object
+
+const Student1 = { 
+    FirstName: "Nelson" ,
+    lastName : "Wilson",
+    PhoneNumber: 60093031302,
+    EmailAddress: "Nelsonwilson@gmail.com",
+    Gender: "Male",
+    Address: "Rumola street",
+    Score: 200%
+    
+""}
+
+console.log(Student1)
+
+
+const Student2 = { 
+    FirstName: "Emmanuel" ,
+    lastName : "Bola",
+    PhoneNumber: 72293030302,
+    EmailAddress: "Emmanuelbola@gmail.com",
+    Gender: "Male",
+    Address: "Rumola street",
+    Score: 250%
+    
+""}
+
+console.log(Student2)
+
+const Student3 = { 
+    FirstName: "Blessing" ,
+    lastName : "Olu",
+    PhoneNumber: 40093030300,
+    EmailAddress: "BlessingOlu@gmail.com",
+    Gender: "female",
+    Address: "Rumola street",
+    Score: 200%
+    
+""}
+
+console.log(Student3)
+
+
+const Student4 = { 
+    FirstName: "Precious" ,
+    lastName : "Mark",
+    PhoneNumber: 20093030302,
+    EmailAddress: "Preciousmark@gmail.com",
+    Gender: "female",
+    Address: "Rumola street",
+    Score: 180%
+    
+""}
+
+console.log(Student4)
+
+
+const Student5 = { 
+    
+    FirstName: "Mattew" ,
+    lastName : "John",
+    PhoneNumber: 90095930302,
+    EmailAddress: "Mattewjohn@gmail.com",
+    Gender: "Male",
+    Address: "Rumola street",
+    Score: 350%
+    
+ ""}
+
+console.log(Student5)
+
+
+
+// Array
+
+
+
+
+const Phones = ["IPhone", "Vivo", "Techno", "Oppo"]
+
+const types = ["15promax", "Y1s", "Camoon19", "P367"]
+
+console.log(Phones)
+
+console.log(types)
+
+
+
+const carbrands =["BMW", "G Wagon", "Mazda","Pigeot"]
+
+
+
+console.log(carbrands)
+
+
+
+const Clothesbrands = ["Gucci",
+ "Fendi", 
+ "Lois vuton",
+  "Balenciaga"]
+
+
+  console.log(Clothesbrands)
+
+
+  const Laptopbrands = ["HP","APPLE", "LENOVO"]
+
+  console.log(Laptopbrands)
+
+  const houses = ["bungalow", "hot","mansion","duplex"]
+
+  console.log(houses)
+
+
+//   if or else
+
+const Score = 45
+
+
+if (Score <= 45) {
+console.log("fail")
+
+}
+if (Score >49 <60) {
+        
+}
+console.log("pass")
+
+if (Score > 59 < 69) {
+    
+}
+console.log("very good")
+
+if (Score >69) {
+    
+}
+console.log("Excellent")
+
+ 
